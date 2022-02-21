25ffd89ccbaa0032ecf4a35f628cce6932486e27
Tim <tymurprokhorov@gmail.com>
2022-02-21 11:54:19 +0200

    configure methods fixed

--- RickAndMorty/src/main/java/com/tim/rickandmorty/SecurityConfiguration.java
+++ RickAndMorty/src/main/java/com/tim/rickandmorty/SecurityConfiguration.java
@@ -2,21 +2,20 @@ package com.tim.rickandmorty;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends SecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-
- //   @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception { //метод-источник данных для определения юзеров
-        auth
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+                auth
                 .inMemoryAuthentication()  //пароль будет сохраняться каждый раз, как апп стартует(удаляется при закрытии апп)
                 .withUser("Admin").password(passwordEncoder().encode("Admin123")).roles("ADMIN")
                 .and()
@@ -25,14 +24,15 @@ public class SecurityConfiguration extends SecurityConfigurerAdapter {
                 .withUser("Morty").password(passwordEncoder().encode("Morty123")).roles("USER");
     }
 
- //   @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception { //метод для аутент.запросов
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .authorizeRequests()
                 .anyRequest().authenticated() //доступ только авторизованным юзерам.      Любой запрос
                 .and()
                 .httpBasic(); //используем базовую аут
     }
+
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
